3c170857ae7d2b8a3bf900ef5936f780f519c765
bijbrengen <slagter@bijbrengen.nl>
2019-09-29 20:38:07 +0200

    Started with oracle

--- index.php
+++ index.php
@@ -1,72 +1,3 @@
-<?
-class Question
-{
-    private $text;
-    private $answers = array();
-    public function __construct( $text )
-    {
-        $this->text = $text;
-    }
-
-    public function set_answer( $text, $response )
-    {
-        $this->answers[] = new Answer( $text, $response );
-    }
-
-    public function get_answers()
-    {
-        return $this->answers;
-    }
-
-    public function get_text()
-    {
-        return $this->text;
-    }
-
-}
-
-class Answer
-{
-   public $text;
-   private $response;
-
-   public function __construct( $text, $response )
-   {
-       $this->text = $text;
-       $this->response = new Response( $response );
-   }
-
-   public function get_response()
-   {
-       return $this->response->get_weighted_random();
-   }
-}
-
-class Response
-{
-    private $next_questions = array();
-
-    public function __construct( $response )
-    {
-        $this->next_questions = $response;
-    }
-
-    public function get_weighted_random()
-    {
-        $sum_of_all_weights = array_sum( $this->next_questions );
-        $random_from_weights = rand( 1, $sum_of_all_weights );
-        foreach ( $this->next_questions as $next_question => $weight )
-        {
-            $random_from_weights -= $weight;
-            if ( $random_from_weights <= 0 )
-            {
-                return $next_question;
-            }
-        }
-        return false;
-    }
-}
-?>
 <!DOCTYPE html>
 <html lang="en-US">
 <head>
@@ -142,6 +73,75 @@ foreach ( $question->get_answers() as $answer )
 echo '            <li class="answer_button_add"><a href="mailto:oracle@leerparadijs.nl?SUBJECT='. $title_url . '&BODY=Beste%20Oracle,%0DGraag%20het%20volgende%20antwoord%20toevoegen:">Voeg jouw antwoord toe!</a></li>' . PHP_EOL;
 echo '        </ul>' . PHP_EOL;
 echo '    </main>' . PHP_EOL;
+
+
+class Question
+{
+    private $text;
+    private $answers = array();
+    public function __construct( $text )
+    {
+        $this->text = $text;
+    }
+
+    public function set_answer( $text, $response )
+    {
+        $this->answers[] = new Answer( $text, $response );
+    }
+
+    public function get_answers()
+    {
+        return $this->answers;
+    }
+
+    public function get_text()
+    {
+        return $this->text;
+    }
+
+}
+
+class Answer
+{
+   public $text;
+   private $response;
+
+   public function __construct( $text, $response )
+   {
+       $this->text = $text;
+       $this->response = new Response( $response );
+   }
+
+   public function get_response()
+   {
+       return $this->response->get_weighted_random();
+   }
+}
+
+class Response
+{
+    private $next_questions = array();
+
+    public function __construct( $response )
+    {
+        $this->next_questions = $response;
+    }
+
+    public function get_weighted_random()
+    {
+        $sum_of_all_weights = array_sum( $this->next_questions );
+        $random_from_weights = rand( 1, $sum_of_all_weights );
+        foreach ( $this->next_questions as $next_question => $weight )
+        {
+            $random_from_weights -= $weight;
+            if ( $random_from_weights <= 0 )
+            {
+                return $next_question;
+            }
+        }
+        return false;
+    }
+}
 ?>
 </body>
 </html>
