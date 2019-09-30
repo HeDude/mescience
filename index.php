c7f0bd94ebf2d738941d2a51ba3c3edd6aca61e0
bijbrengen <slagter@bijbrengen.nl>
2019-09-30 18:37:43 +0200

    Removed direct use of controller (PHP library). Only content through api

--- index.php
+++ index.php
@@ -12,84 +12,73 @@
 </head>
 <body>
 <?php
-if
-(
-    empty( getenv ( 'PHP_LIBRARY_PATH' ) )
-)
-{
-    exit;
-}
-define( "LIBRARY_PHP_PATH", getenv ( 'PHP_LIBRARY_PATH' ) . "/" );
-require_once LIBRARY_PHP_PATH. "urlrequest.php";
 
-$URLrequest = new URLrequest();
-$url = $URLrequest->get();
-if ( empty( $url ) )
+$title_url = htmlspecialchars( $_GET["title"] );
+$title = "Oeps, er is iets mis met de url!";
+$main = "            <li><h2>Pas de url aan!</h2></li>" . PHP_EOL;
+
+if ( is_string( $title_url ) && !empty( $title_url ) )
 {
-    return false;
-}
-if ( !array_key_exists( "query", $url ) || !array_key_exists( "title", $url[ "query" ] ) )
-{
-    return false;
-}
-$title_url = $url[ "query" ][ "title" ];
-$api = file_get_contents( "https://api.hedude.com/question/json/nl?title=" . $title_url );
-$answers = json_decode
-(
-    utf8_encode
+    $api = file_get_contents( "https://api.hedude.com/question/json/nl?title=" . $title_url );
+
+    $answers = json_decode
     (
-        $api
-    ),
-    JSON_OBJECT_AS_ARRAY
-);
-if ( empty ( $answers ) )
-{
-    echo '    <header>' . PHP_EOL;
-    echo '        <img src="image/socrates.svg" alt="Socrates">' . PHP_EOL;
-    echo '        <h1>Je hebt mijn vragendoolhof verlaten</h1>' . PHP_EOL;
-    echo '    </header>' . PHP_EOL;
-    return false;
+        utf8_encode
+        (
+            $api
+        ),
+        JSON_OBJECT_AS_ARRAY
+    );
+    $title = "Je hebt mijn vragendoolhof verlaten";
+    if ( !empty ( $answers ) )
+    {
+        $question = new Question( $answers[ "title" ][ "title" ] );
+        $title = $question->get_text();
+
+
+        if ( $answers[ "title" ][ "choice" ] )
+        {
+            foreach( $answers[ "title" ][ "choice" ] as $choice )
+            {
+                $question->set_answer( $choice[ "answer" ], $choice[ "response" ] );
+            }
+            $main = "            <li><h2>Klik op jouw antwoord!</h2></li>" . PHP_EOL;
+        }
+        else
+        {
+            $main = '            <li><h2>Gefeliciteerd, ik kan je geen mogelijke antwoorden meer geven!</h2></li>' . PHP_EOL;
+        }
+
+        foreach ( $question->get_answers() as $answer )
+        {
+            $response = urlencode( $answer->get_response() );
+            if ( empty( $response ) )
+            {
+                $main .= '            <li class="answer_button_off">' . $answer->text;
+            }
+            else
+            {
+                $main .= '            <li class="answer_button"><a href="?title=' . $response . '">' . $answer->text . '</a>';
+            }
+            $main .= '</li>' . PHP_EOL;
+        }
+        $main .= '            <li class="answer_button_add"><a href="mailto:oracle@leerparadijs.nl?SUBJECT='. $title_url . '&BODY=Beste%20Oracle,%0DGraag%20het%20volgende%20antwoord%20toevoegen:">Voeg jouw antwoord toe!</a></li>' . PHP_EOL;
+    }
 }
-
-$question = new Question( $answers[ "title" ][ "title" ] );
-$title = $question->get_text();
-
 echo '    <header>' . PHP_EOL;
 echo '        <img src="image/socrates.svg" alt="Socrates">' . PHP_EOL;
 echo '        <h1>' . $title . '</h1>' . PHP_EOL;
 echo '    </header>' . PHP_EOL;
 echo '    <main>' . PHP_EOL;
 echo '        <ul>' . PHP_EOL;
-
-if ( $answers[ "title" ][ "choice" ] )
-{
-    foreach( $answers[ "title" ][ "choice" ] as $choice )
-    {
-        $question->set_answer( $choice[ "answer" ], $choice[ "response" ] );
-    }
-}
-else
-{
-    echo '            <li><h2>Gefeliciteerd, ik kan je geen mogelijke antwoorden meer geven!</h2></li>' . PHP_EOL;
-}
-
-foreach ( $question->get_answers() as $answer )
-{
-    $response = urlencode( $answer->get_response() );
-    if ( empty( $response ) )
-    {
-        echo '            <li class="answer_button_off">' . $answer->text;
-    }
-    else
-    {
-        echo '            <li class="answer_button"><a href="?title=' . $response . '">' . $answer->text . '</a>';
-    }
-    echo '</li>' . PHP_EOL;
-}
-echo '            <li class="answer_button_add"><a href="mailto:oracle@leerparadijs.nl?SUBJECT='. $title_url . '&BODY=Beste%20Oracle,%0DGraag%20het%20volgende%20antwoord%20toevoegen:">Voeg jouw antwoord toe!</a></li>' . PHP_EOL;
+echo              $main;
 echo '        </ul>' . PHP_EOL;
 echo '    </main>' . PHP_EOL;
 
+?>
+</body>
+</html>
+<?php
 
 class Question
 {
@@ -158,6 +147,3 @@ class Response
         return false;
     }
 }
-?>
-</body>
-</html>
