018df7808e68fe2608802b2e4f4c86822b46a786
Bijbrengen <slagter@bijbrengen.nl>
2019-11-16 15:06:33 +0100

    Read guides from json file

--- controller/program/mescience.php
+++ controller/program/mescience.php
@@ -1,12 +1,15 @@
 <?php
 class MeScience
 {
+    private $guides = false;
+
     public function __construct()
     {
     }
 
     public function model( string $config_file, string $log_file ) : bool
     {
+        $this->guides = $this->set_guide( $config_file );
         return true;
     }
 
@@ -18,5 +21,15 @@ class MeScience
             return true;
         }
         return false;
+    }
+
+    private function set_guide( string $guide_file )
+    {
+        if ( !file_exists( $guide_file ) )
+        {
+            return false;
+        }
+        $this->guides = json_decode( utf8_encode( file_get_contents( $guide_file ) ), true );
+        return $this->guides;
     }
 }
--- index.php
+++ index.php
@@ -3,6 +3,6 @@ require_once( 'model/configuration.php');
 require_once( MESCIENCE_PROGRAM_PATH . '/mescience.php' );
 
 $mescience = new MeScience();
-$mescience->model( 'model/philosophers.json', 'model/gamepin.txt' );
+$mescience->model( 'model/guides.json', 'model/gamepin.txt' );
 $mescience->view(  'view/template/mescience.phtml' );
 ?>
