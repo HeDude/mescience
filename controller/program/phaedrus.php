9a0fcaef6c95137f841388a05b3aa9466e6b4b6d
Bijbrengen <slagter@bijbrengen.nl>
2020-03-11 08:08:03 +0100

    Added fabeltjeskrant idea

---
+++ controller/program/phaedrus.php
@@ -0,0 +1,10 @@
+<?php
+//! De fabeltjeskrant
+/* De speler kiest een fabel. De fabels zijn moraliserend, alleen vaak ook oordelend.
+ * De speler wordt uitgenodigd bij zichzelf te kijken of er diep in hem of haar
+ * ook niet de neiging is tot het gedrag wat aan de kaak wordt gesteld.
+ * Welke onderliggende motieven zijn er voor dat gedrag?
+ * De speler wordt uitgedaagd om zo diep te graven dat geen van de medespelers over het motief zelf kunnen oordelen.
+ * Het spel wordt afgerond met een fabel door de speler waarin de verkenning wordt gedeeld met de mensheid.
+ * Mooie fabels komen in de fabeltjeskrant.
+ */
